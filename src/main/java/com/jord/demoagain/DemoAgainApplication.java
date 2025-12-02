294c84f3d6e8a419f908ffe309402be5089d0c65
WORK4 <car@ford.net>
2025-12-02 14:43:00 -0600

    Commiting the second try on the Spring Boot App.

---
+++ src/main/java/com/jord/demoagain/DemoAgainApplication.java
@@ -0,0 +1,13 @@
+package com.jord.demoagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jord/demoagain/DemoAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jord.demoagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
